a64be217f853e9f458f1de88d9ab8ea403d5a164
Путилов Константин <putilov@signatec.ru>
2026-03-01 22:59:19 +0700

    выделен вспомогательный класс SessionHelper

--- test/test_add_entry.py
+++ test/test_add_entry.py
@@ -12,7 +12,7 @@ def app(request):
 
 
 def test_add_entry(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_entry(Entry(firstname="Иван", middlename="Иванович", lastname="Иванов", nickname="косой",
                                 title="заголовок", company="самая лучшая компания", address="адрес компании",
                                 phone_home="73831234567", phone_mobile="79139130001", phone_work="73831122334",
@@ -20,4 +20,4 @@ def test_add_entry(app):
                                 homepage_url="home page url",
                                 birth_day="27", birth_month="December", birth_year="1988",
                                 anniversary_day="28", anniversary_month="November", anniversary_year="2000"))
-    app.logout()
+    app.session.logout()
