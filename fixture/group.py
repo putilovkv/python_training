8b9ccee263aa63d0a8d2d75b79fcfb6931583a30
Путилов Константин <putilov@signatec.ru>
2026-03-02 18:10:09 +0700

    добавлен тест на удаление первой группы фикстура вынесена в отдельный файл conftest.py

--- fixture/group.py
+++ fixture/group.py
@@ -23,3 +23,12 @@ class GroupHelper:
         # submit group creation
         wd.find_element_by_name("submit").click()
         self.app.navigation.go_to_groups_page()
+
+    def delete_first_group(self):
+        wd = self.app.wd
+        self.app.navigation.go_to_groups_page()
+        # select first group
+        wd.find_element_by_name("selected[]").click()
+        # submit deletion
+        wd.find_element_by_name("delete").click()
+        self.app.navigation.go_to_groups_page()
--- test/test_add_entry.py
+++ test/test_add_entry.py
@@ -1,14 +1,5 @@
 # -*- coding: utf-8 -*-
-import pytest
 from model.entry import Entry
-from fixture.application import Application
-
-
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
 
 
 def test_add_entry(app):
