b24b2db8aa128b17d9bd520732f39cc3484d34ea
Путилов Константин <putilov@signatec.ru>
2026-03-07 20:05:31 +0700

    Реализована проверка нахождения на страницах, чтобы не переходить к ним повторно. Убрано ожидание появления элементов (5 секунд)

--- fixture/navigation.py
+++ fixture/navigation.py
@@ -3,14 +3,18 @@ from fixture.base_helper import BaseHelper
 
 class NavigationHelper(BaseHelper):
 
+    home_page = "http://localhost/addressbook"
+
     def open_home_page(self):
         wd = self.app.wd
-        wd.get("http://localhost/addressbook/")
+        wd.get(self.home_page)
 
     def go_to_home_page(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("home").click()
+        if wd.current_url.rstrip('/') != self.home_page or not wd.current_url.endswith("/index.php"):
+            wd.find_element_by_link_text("home").click()
 
     def go_to_groups_page(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("groups").click()
+        if not wd.current_url.endswith("/groups.php") or len(wd.find_elements_by_name("new")) == 0:
+            wd.find_element_by_link_text("groups").click()
