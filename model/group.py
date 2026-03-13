09176f6c987f0f8644c9202f3087928288d90588
Путилов Константин <putilov@signatec.ru>
2026-03-13 22:46:22 +0700

    реализовано сравнение и сортировка групп в тесты по группам добавлены проверки на равенство списков

--- model/group.py
+++ model/group.py
@@ -1,3 +1,4 @@
+from sys import maxsize
 
 
 class Group:
@@ -7,3 +8,25 @@ class Group:
         self.header = header
         self.footer = footer
         self.id = id
+
+    def __repr__(self):
+        return f"{self.id}:{self.name}"
+
+    def __eq__(self, other):
+        return (self.id is None or other.id is None or self.id == other.id) and self.name == other.name
+
+    def id_or_max(self):
+        if self.id:
+            return int(self.id)
+        else:
+            return maxsize
+
+    def fill_if_none(self, other):
+        if self.name is None:
+            self.name = other.name
+        if self.header is None:
+            self.header = other.header
+        if self.footer is None:
+            self.footer = other.footer
+        if self.id is None:
+            self.id = other.id
--- test/test_mod_group.py
+++ test/test_mod_group.py
@@ -7,32 +7,48 @@ def test_modify_first_group(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test_modify_first_group"))
     old_groups = app.group.get_group_list()
-    app.group.modify_first_group(Group(name=f"new_grname_{datetime.now().strftime('%H:%M:%S.%f')}",
-                                       header=f"new grheader_{datetime.now().strftime('%H:%M:%S.%f')}",
-                                       footer=f"new grfooter_{datetime.now().strftime('%H:%M:%S.%f')}"))
+    group = Group(name=f"new_grname_{datetime.now().strftime('%H:%M:%S.%f')}",
+                  header=f"new grheader_{datetime.now().strftime('%H:%M:%S.%f')}",
+                  footer=f"new grfooter_{datetime.now().strftime('%H:%M:%S.%f')}")
+    group.fill_if_none(old_groups[0])
+    app.group.modify_first_group(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 def test_modify_group_name(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test_modify_group_name"))
     old_groups = app.group.get_group_list()
-    app.group.modify_first_group(Group(name=f"new_grname_{datetime.now().strftime('%H:%M:%S.%f')}"))
+    group = Group(name=f"new_grname_{datetime.now().strftime('%H:%M:%S.%f')}")
+    group.fill_if_none(old_groups[0])
+    app.group.modify_first_group(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 def test_modify_group_header(app):
     if app.group.count() == 0:
         app.group.create(Group(header="test_modify_group_header"))
     old_groups = app.group.get_group_list()
-    app.group.modify_first_group(Group(header=f"new_grheader_{datetime.now().strftime('%H:%M:%S.%f')}"))
+    group = Group(header=f"new_grheader_{datetime.now().strftime('%H:%M:%S.%f')}")
+    group.fill_if_none(old_groups[0])
+    app.group.modify_first_group(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 def test_modify_group_footer(app):
     if app.group.count() == 0:
         app.group.create(Group(footer="test_modify_group_footer"))
     old_groups = app.group.get_group_list()
+    group = Group(footer=f"new_grfooter_{datetime.now().strftime('%H:%M:%S.%f')}")
+    group.fill_if_none(old_groups[0])
     app.group.modify_first_group(Group(footer=f"new_grfooter_{datetime.now().strftime('%H:%M:%S.%f')}"))
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
