4ae9c981a9792594caf978ffaddab5bcfc8b2d46
Путилов Константин <putilov@signatec.ru>
2026-03-13 19:17:22 +0700

    +

--- test/test_mod_group.py
+++ test/test_mod_group.py
@@ -13,7 +13,6 @@ def test_modify_first_group(app):
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
 
-
 def test_modify_group_name(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test_modify_group_name"))
